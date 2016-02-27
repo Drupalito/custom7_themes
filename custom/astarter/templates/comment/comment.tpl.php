c2165a8cbd68ca6bc1c6e891589f04326f05eb44
aquelito <axel@aquelito.fr>
2016-02-27 21:29:43 +0100

    Comments: fix some errors

--- custom/astarter/templates/comment/comment.tpl.php
+++ custom/astarter/templates/comment/comment.tpl.php
@@ -7,36 +7,40 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728216
  */
-?>
-<div class="<?php print $classes; ?> row"<?php print $attributes; ?>>
-  <div class="comment__header mbs col-xs-12 col-sm-3">
-    <p class="mvn">
-      <?php if ($new): ?>
-        <mark class="mark mark--new"><?php print $new; ?></mark>
-      <?php endif; ?>
-      <?php if ($status == 'comment-unpublished'): ?>
-        <mark class="mark mark--unpublished"><?php print t('Unpublished'); ?></mark>
-      <?php endif; ?>
-      <?php if ($submitted): ?>
-        <cite itemprop="creator name"><?php print $author; ?></cite><br /><time datetime="<?php print format_date($comment->created, 'custom', 'c'); ?>" itemprop="commentTime"><?php print $created; ?></time>
-        <?php if ($comment->created != $comment->changed): ?>
-          <time datetime="<?php print format_date($comment->changed, 'custom', 'c'); ?>"><?php print $changed; ?></time>
-        <?php endif; ?>
-    <?php endif; ?>
-    </p>
-  </div>
-  <div class="comment__description col-xs-12 col-sm-9">
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['links']);
-      print render($content);
-    ?>
-    <?php print render($content['links']); ?>
-    <?php if ($signature): ?>
-      <footer class="user-signature comment__signature clearfix">
-        <?php print $signature; ?>
-      </footer>
-    <?php endif; ?>
-  </div>
 
+hide($content['links']);
+?>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div>
+    <div class="comment__header">
+      <p class="mvn">
+
+        <?php if ($submitted): ?>
+          <?php print $picture ?>
+          <span class="inbl amiddle">
+            <cite itemprop="creator name"><span class="bold"><?php print $author; ?></span></cite>
+            <?php if ($new): ?>
+               <mark class="mark mark--new"><?php print $new; ?></mark>
+            <?php endif; ?>
+            <?php if ($status == 'comment-unpublished'): ?>
+               <mark class="mark mark--unpublished"><?php print t('Unpublished'); ?></mark>
+            <?php endif; ?>
+            <br /><time class="small" datetime="<?php print format_date($comment->created, 'custom', 'c'); ?>" itemprop="commentTime"><?php print $created; ?></time>
+            <?php if ($comment->created != $comment->changed): ?>
+              <time class="small" datetime="<?php print format_date($comment->changed, 'custom', 'c'); ?>"><?php print $changed; ?></time>
+            <?php endif; ?>
+          </span>
+      <?php endif; ?>
+      </p>
+    </div>
+    <div class="comment__description">
+      <?php print render($content); ?>
+      <?php if ($signature): ?>
+        <footer class="comment__signature user-signature clearfix">
+          <?php print $signature; ?>
+        </footer>
+      <?php endif; ?>
+      <?php print render($content['links']); ?>
+    </div>
+  </div>
 </div>
