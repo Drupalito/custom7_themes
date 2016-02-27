6f185abe54261795b4a2bf3e1c2f53a1c7f1e845
aquelito <axel@aquelito.fr>
2016-02-27 21:32:03 +0100

    ckeditor: responsive width

--- custom/subtheme_child/ckeditor.config.js
+++ custom/subtheme_child/ckeditor.config.js
@@ -16,7 +16,7 @@ CKEDITOR.editorConfig = function(config) {
   config.justifyClasses = [ 'text-left', 'text-center', 'text-right', 'text-justify' ];
 
   // The minimum editor width, in pixels, when resizing it with the resize handle.
-  config.resize_minWidth = 450;
+  config.resize_minWidth = 320;
 
   // Protect PHP code tags (<?...?>) so CKEditor will not break them when
   // switching from Source to WYSIWYG.
@@ -29,72 +29,4 @@ CKEDITOR.editorConfig = function(config) {
   // Code tags
   config.protectedSource.push(/<code>[\s\S]*?<\/code>/gi);
   config.extraPlugins = '';
-
-  /**
-   * Append here extra CSS rules that should be applied into the editing area.
-   * Example:
-   * config.extraCss = 'body { color:#FF0000; }';
-   */
-  config.extraCss = '';
-
-  /**
-   * Sample extraCss code for the "marinelli" theme.
-   */
-  if (Drupal.settings.ckeditor.theme == "newtheme_sub") {
-    config.extraCss += "";
-  }
-
-  /**
-   * CKEditor's editing area body ID & class.
-   * See http://drupal.ckeditor.com/tricks
-   * This setting can be used if CKEditor does not work well
-   * with your theme by default.
-   */
-  config.bodyClass = '';
-  config.bodyId = '';
-
-  /**
-   * Sample bodyClass and BodyId for the "marinelli" theme.
-   */
-  if (Drupal.settings.ckeditor.theme == "newtheme_sub") {
-    config.bodyClass = 'singlepage';
-    config.bodyId = 'primary';
-  }
 }
-
-/**
- * Sample toolbars
- */
-
-// Toolbar definition for basic buttons
-Drupal.settings.cke_toolbar_DrupalBasic = [ [ 'Format', 'Bold', 'Italic', '-', 'NumberedList','BulletedList', '-', 'Link', 'Unlink', 'Image' ] ];
-
-// Toolbar definition for Advanced buttons
-Drupal.settings.cke_toolbar_DrupalAdvanced = [
-  ['Source'],
-  ['Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker'],
-  ['Undo','Redo','Find','Replace','-','SelectAll','RemoveFormat'],
-  ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','Iframe'],
-  ['Maximize', 'ShowBlocks'],
-  '/',
-  ['Format'],
-  ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-  ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
-  ['JustifyLeft','JustifyCenter','JustifyRight','-','BidiRtl','BidiLtr'],
-  ['Link','Unlink']
-];
-
-// Toolbar definiton for all buttons
-Drupal.settings.cke_toolbar_DrupalFull = [
-  ['Source'],
-  ['Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker'],
-  ['Undo','Redo','Find','Replace','-','SelectAll','RemoveFormat'],
-  ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','Iframe'],
-  '/',
-  ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
-  ['NumberedList','BulletedList','-','Indent','Blockquote','CreateDiv'],
-  ['JustifyLeft','JustifyCenter','JustifyRight','-','BidiRtl','BidiLtr'],
-  ['Link','Unlink'],
-  '/',
-  ['Format','Font','FontSize'],
-];
