caefc6e42f72b0e90fdb2a52904a121e8463d068
aquelito <axel@aquelito.fr>
2016-05-15 14:53:46 +0200

    Taxonomy fix attribute title

--- astarter/templates/taxonomy/taxonomy-term.tpl.php
+++ astarter/templates/taxonomy/taxonomy-term.tpl.php
@@ -14,7 +14,7 @@
 ?>
 <div<?php print $attributes; ?>>
   <?php if (!$page): ?>
-    <h2<?php print $content_attributes_title; ?>><a href="<?php print $term_url; ?>"><?php print $term_name; ?></a></h2>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $term_url; ?>"><?php print $term_name; ?></a></h2>
   <?php endif; ?>
   <div<?php print $content_attributes; ?>>
     <?php print render($content); ?>
